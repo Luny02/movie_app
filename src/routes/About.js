a91649b6bdfb11b2553718ead274bf18cdade8b0
Luny02 <lunyguy@gmail.com>
2023-06-10 01:06:31 +0600

    first commit

---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function About () {
+//     // return <span>О проекте: Создан по мотиву Салибаева Айжигита т.к любит фильмы </span>
+// };
+
+
+
+export default About;
